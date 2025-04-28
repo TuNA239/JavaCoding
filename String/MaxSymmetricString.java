d2acb6ed74a126a53c825de3ddc912bc2ab47059
TuNA <nguyentu2392@gmail.com>
2025-04-28 22:12:32 +0700

    fix: max length sub symmetric string

--- String/MaxSymmetricString.java
+++ String/MaxSymmetricString.java
@@ -4,37 +4,37 @@ import java.util.Scanner;
 
 public class MaxSymmetricString {
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = Integer.parseInt(sc.nextLine());
+		Scanner sc = new Scanner(System.in);
+		int T = Integer.parseInt(sc.nextLine());
+		for (int test = 1; test <= T; test++) {
+			String s = sc.nextLine();
+			int n = s.length();
+			boolean[][] dp = new boolean[n][n];
+			int maxLength = 1;
 
-        for (int test = 1; test <= T; test++) {
-            String s = sc.nextLine();
-            int n = s.length();
-            int[][] dp = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				dp[i][i] = true;
+			}
 
-            for (int i = 0; i < n; i++) {
-                dp[i][i] = 1;
-            }
+			for (int i = 0; i < n - 1; i++) {
+				if (s.charAt(i) == s.charAt(i + 1)) {
+					dp[i][i + 1] = true;
+					maxLength = 2;
+				}
+			}
 
-            for (int len = 2; len <= n; len++) {
-                for (int i = 0; i <= n - len; i++) {
-                    int j = i + len - 1;
+			for (int len = 3; len <= n; len++) {
+				for (int i = 0; i <= n - len; i++) {
+					int j = i + len - 1;
+					if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+						dp[i][j] = true;
+						maxLength = len;
+					}
+				}
+			}
 
-                    if (s.charAt(i) == s.charAt(j)) {
-                        if (len == 2) {
-                            dp[i][j] = 2;
-                        } else {
-                            dp[i][j] = dp[i + 1][j - 1] + 2;
-                        }
-                    } else {
-                        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-                    }
-                }
-            }
-
-            System.out.println("#" + test + " " + dp[0][n - 1]);
-        }
-
-        sc.close();
-    }
+			System.out.println("#" + test + " " + maxLength);
+		}
+		sc.close();
+	}
 }
